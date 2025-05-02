abc620f11ddac939ef5fe350d67e6609e5433e75
Manish9917 <manishmaurya12798@gmail.com>
2025-05-02 14:38:37 +0530

    fix: removed buffCommands from mongoose config

--- config/db.js
+++ config/db.js
@@ -1,32 +1,25 @@
 import mongoose from "mongoose";
 // import { cache } from "react";
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, Promise: null }
+    cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
-
     if (cached.conn) {
-        return cached.conn
+        return cached.conn;
     }
 
     if (!cached.promise) {
-        const opts = {
-            buffCommands: false
-        }
-
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then(mongoose => {
-            return mongoose
-        })
-
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`).then((mongoose) => {
+            return mongoose;
+        });
     }
 
-    cached.conn = await cached.promise
-    return cached.conn
-
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
 
-export default connectDB
+export default connectDB;
